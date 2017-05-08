e9d1c259c044dc110f348367fe57dcfb951c251d
Dave C <dcohen18@gmail.com>
2017-05-08 14:20:44 -0500

    Create backup-manager.py

---
+++ backup-manager.py
@@ -0,0 +1,2 @@
+# this defines a BackupManager class that glues together all other pieces 
+# performing scan of dir and subdirs + criteria checking + backup if necessary, with the values from settings.conf
