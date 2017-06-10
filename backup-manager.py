9472e9b58743e08da08f5fe869505899d861018d
Dave C <dcohen18@gmail.com>
2017-06-10 14:16:27 -0500

    pseudo-code for review

--- backup-manager.py
+++ backup-manager.py
@@ -1,2 +1,16 @@
 # this defines a BackupManager class that glues together all other pieces 
 # performing scan of dir and subdirs + criteria checking + backup if necessary, with the values from settings.conf
+
+from scanner import *
+from criteria import *
+from backup import *
+import sys
+import os
+
+myDir = "/Users/davecohen/Documents/DEC/Programming/-PYTHON/__get__lessons"
+backupDir = "/Users/davecohen/MEGA/Backups-Mega/backup-test"
+myFilter = ExtensionFilter().filter(filename, folderName, 'txt')
+
+for filename, folderName in Scan(myDir):
+    if filename in myFilter():
+        Backup(os.path.join(filename, folderName), backupDir)
