c96ed9bfa5d6b96963d8f9806a566cc368822b2c
Dave C <dcohen18@gmail.com>
2017-06-10 14:18:49 -0500

    now a generator

--- scanner.py
+++ scanner.py
@@ -2,34 +2,12 @@
 
 from os import walk
 
-### DON'T USE dir as a variable/argument name, or you'll override the builtin function dir() in the local namespace !!
-
-def Scan(directory):
+def Scan(dir):
     """
     Given the path to a directory, it will return an iterable containing tuples (filename, subpath)
     :param dir: path to the directory to scan
     :return: 
     """
-    filelist = []
-    for folderName, subfolders, filenames in walk(directory):
-        #print('The current folder is ' + folderName)
+    for folderName, subfolders, filenames in walk(dir):
         for filename in filenames:
-            filelist.append((filename, folderName))
-    ### This could also be made into a generator, let's maybe talk about it next lesson?
-    return filelist
-
-### LET'S TALK ABOUT HOW TO DO TESTS NEXT TIME :)
-#test Scan
-for data in Scan(directory):
-    for folderName, subfolders, filenames in walk(directory):
-#         print('The current folder is ' + folderName)
-        for subfolder in subfolders:
-            pass
-#             print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
-        for filename in filenames:
-            filelist.append((filename, folderName))
-    return filelist
-
-#test Scan
-for data in Scan(my_dir):
-    print(data)
+            yield (filename, folderName)
